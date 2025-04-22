df96970a39850ae8c045122d18eefc4e068b05e7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-04-22 09:17:59 +0200

    Add JSON-LD schema and enhance metadata in layout
    Introduced structured data using JSON-LD to improve SEO and provide detailed information about the site owner. Updated metadata keywords for better search engine discoverability and added the necessary script to the layout via the Next.js Head component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,11 +11,23 @@ import "./styles/projects.css";
 
 import QueryProvider from "@/app/context/QueryProvider";
 import {ThemeProvider} from "@/app/context/ThemeProvider";
+import Head from "next/head";
 
 export const metadata: Metadata = {
     title: "Olivier Van D'huynslager",
     description: "Personal website and portfolio of Olivier Van D'huynslager; a curator and strategist based in Gent, Belgium. Currently working at Design Museum Gent, his work is driven by the intricate relationships between digital technology and design.",
-    keywords: ["Olivier Van D'huynslager", "Curator", "curator", "design", "digital", "culture"],
+    keywords: [
+        "Olivier Van D'huynslager",
+        "oliviervandhuynslager",
+        "Olivier Dhuynslager",
+        "digital design strategist",
+        "portfolio",
+        "Curator",
+        "curator",
+        "design",
+        "digital",
+        "culture"
+    ],
     openGraph: {
         title: "Olivier Van D'huynslager",
         description: "Personal website and portfolio of Olivier Van D'huynslager; a curator and strategist based in Gent, Belgium. Currently working at Design Museum Gent, his work is driven by the intricate relationships between digital technology and design.",
@@ -36,8 +48,28 @@ export default function RootLayout({
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Person",
+        "name": "Olivier Van D'huynslager",
+        "alternateName": "oliviervandhuynslager",
+        "url": "https://oliviervandhuynslager.net",
+        "jobTitle": "Curator and Digital Strategist",
+        "affiliation": {
+            "@type": "Organization",
+            "name": "Design Museum Gent",
+        },
+    };
+
+
     return (
         <html lang="en">
+        <Head>
+            <script type="application/ld+json"
+                    dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+            ></script>
+        </Head>
         <body>
         <ThemeProvider>
             {/* Wrap your entire app with QueryClientProvider */}
